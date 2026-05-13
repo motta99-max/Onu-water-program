c99801df1694d1853e37aab230082b3c5d35d064
motta99-max <aa00011562@my.uvm.edu.mx>
2026-05-13 09:06:05 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const mensajeAgua = "💧 La ONU informa: El agua es vida. Tú puedes marcar la diferencia hoy. Bienvenido a la comunidad de ciudadanos responsables. 🌍";
+
+const contenedorMarquesina = document.getElementById('marquesina-dinamica');
+contenedorMarquesina.innerText = mensajeAgua;
+
+console.log("Mensaje de bienvenida tipo marquesina cargado correctamente.");
